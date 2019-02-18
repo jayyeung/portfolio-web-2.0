900298d4e9d5e1591dfbfa99ef430e02089d4455
Jason Yeung <jsy9@sfu.ca>
2019-02-18 14:55:23 -0800

    add feature shoutout

--- src/components/partials/intro.js
+++ src/components/partials/intro.js
@@ -8,17 +8,37 @@ import Social from '../blocks/Social';
 import Container from '../elements/Container';
 import Label from '../elements/Label';
 import Link from '../elements/Link';
+
 const Header = styled.header`
   ${tw`flex flex-wrap mt-60 md:mt-80 lg:mt-120 pt-120`}
 `;
 
+const Ref = styled.span`
+  ${tw`text-primary capitalize`}
+`;
+
 
 // Main
+const Shoutout = (() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const ref = localStorage.getItem('ref') || urlParams.get('ref');
+  // Ceasar shifts 13
+  const decode = str => (str.toUpperCase().replace(/[A-Z]/g, C => (
+    String.fromCharCode((C.charCodeAt(0) % 26) + 65)))).toLowerCase();
+  
+  const decodeRef = decode(ref);
+  if (ref && decodeRef.slice(-6) === 'jyeung') {
+    localStorage.setItem('ref', ref);
+    return decodeRef.slice(0, decodeRef.length-6);
+  }
+})();
+
 const Intro = ({ data }) => (
   <Container id='intro'>
     <Header>
       <h1 className='w-full'>
-        Welcome there!<br/>
+        { (Shoutout) ? <span>Hey <Ref>{Shoutout}</Ref>!</span> : 'Welcome there!'}
+        <br/>
         My name's <span className='text-primary inline-block'>{data.name},</span><br/>
         I am an aspiring Front-end Developer.
       </h1>
